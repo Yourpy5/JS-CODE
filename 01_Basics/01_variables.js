1e6bff47122e7f79ec29c99ab4916b89dcdc6d76
Yourpy5 <166502415+Yourpy5@users.noreply.github.com>
2024-10-15 10:26:32 +0000

    container config done

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountID = 21
+let accountEmail = "abc@gg.com"
+var accountName = "Geller"
+accountCity = "Mysore"
+let accountPeople /* it will print undefined */
+console.log(accountEmail);
+
+// accountID = 4 not allowed in const
+
+console.log(accountID);
+
+console.table([accountName, accountID, accountEmail, accountCity, accountPeople])
